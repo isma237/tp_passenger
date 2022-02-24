fd18d3dc68a6b18779c13181a4ced094c52465bd
zabps11 <zgphilibert11@gmail.com>
2022-02-24 17:27:38 +0100

    ajout de fonction ,traduction ,ajout du menu

--- main.py
+++ main.py
@@ -13,23 +13,25 @@ passengerTemp = {
 busTemp = {
     "id" : "",
     "availablePlace" : 0,
+    "nbreKg":0.0,
     "listPassenger" : []
 }
 
 myListBus = []
 myVacanPassgerList = []
+response="m"
 #---------------------------------FONCTIONS DEFINITION--------------------------------
 def createBus(): #creation d'un bus
     response=''
-    print("___Create a new Bus___")
+    print("___________Create a new Bus___________")
     while response != 'n':
-
+        print()
         currentBus = copy.deepcopy(busTemp)
-        currentBus["id"] = input("==> Enter the bus ID :  ")
+        currentBus["id"] = input("==> Enter the bus ID :  ").lower()
         for bus in myListBus:
             while bus["id"]==currentBus["id"]:
                 print("another bus has already the same ID")
-                currentBus["id"]=input("    Enter the bus ID:  ")
+                currentBus["id"]=input("    Enter the bus ID:  ").lower()
         currentBus["availablePlace"] = int(input("    Enter the possible seat :  "))
         myListBus.append(currentBus)
         response = input("create another bus ? y/n___  ")
@@ -41,9 +43,9 @@ def createPassenger(): #creation d'un passager
     
     response = ''
     id=0
-    print("___Create a new Passenger___")
+    print("___________Create a new Passenger__________")
     while response != 'n':
-        
+        print()
         currentPassenger = copy.deepcopy(passengerTemp)
         currentPassenger["name"] = input("==> Entrer his name :  ").lower()
         for char in currentPassenger["name"]:
@@ -58,48 +60,57 @@ def addPassengerInBus(passengerName , busId): #ajout d'un passager dans un bus
     response='r'
     status=0
     curentbus={}
-    while response == "r": 
-        for bus in myListBus:
-            if busId == bus["id"]:
-                curentbus=bus
-            
-        for pssger in myVacanPassgerList:
-            print(pssger)
-            if passengerName.lower() == pssger["name"]:
+
+    for bus in myListBus:
+        if busId == bus["id"]:
+            curentbus=bus
+        
+    for pssger in myVacanPassgerList:
+        if passengerName.lower() == pssger["name"]:
+            if len(curentbus["listPassenger"]) < curentbus["availablePlace"]:
                 curentbus["listPassenger"].append(pssger)
                 pssger["id"]=curentbus["id"] + str(curentbus["listPassenger"].index(pssger))
+                curentbus["nbreKg"] += pssger["lgageWeight"]
                 myVacanPassgerList.remove(pssger)
+                print("==> le passager a ete ajouter avec success dans le bus")
                 status=1
-        if status==0:
-            print("the name entred in incorrect")
+            else:
+                status=2
+    if status==0:
+        print("the name entred in incorrect")
+    if status==2:
+        print("this bus is full ,it can't receive another passenger")
 
-        response=input("Enter the letter R to retry  :  ")
-        print("------------------------------------------------------------")
+    print("------------------------------------------------------------")
             
-
 def movePassengerInBus(passengerName , busId):
-    response='r'
+
     status1=0
     status2=0
     curentbus={}
-    while response == 'r': 
-        for bus in myListBus:
-            if busId == bus["id"]:
-                curentbus=bus
-                status1=1
-        for pssger in curentbus["listPassenger"]:
-            if passengerName.lower() == pssger["name"]:
-                curentbus["listPassenger"].remove(pssger)
-                myVacanPassgerList.append(pssger)
-                status2=1
-        if status2==0:
-            print("the name entred in incorrect")
-        if status1==0:
-            print("the bus id entred in incorrect") 
-
-        response=input("Entrer la lettre R pour reessayer :  ")
+    id=0
     
+    for bus in myListBus:
+        if busId == bus["id"]:
+            curentbus=bus
+            status1=1
+    for pssger in curentbus["listPassenger"]:
+        if passengerName.lower() == pssger["name"]:
+            curentbus["listPassenger"].remove(pssger)
+            for char in pssger["name"]:
+                id += ord(char)
+            pssger["id"]=str(id)
+            curentbus["nbreKg"] -= pssger["lgageWeight"]
+            myVacanPassgerList.append(pssger)
+            status2=1
+    if status1 == 1 and status2 == 0:
+        print("==> il n'existe pas d'utilisateur de ce nom enregistre dans ce bus")
+    elif status2==0:
+        print("==> the name entred in incorrect")
+    elif status1==0:
+        print("==> the bus id entred in incorrect") 
 
+    
 def printVacanListPassger(): #liste des passager vacans
     for passger in myVacanPassgerList:
         print("==>  ID : {}   Name :  {}   Lugage weight :  {}".format(passger["id"],passger["name"].capitalize(),passger["lgageWeight"]))
@@ -111,42 +122,157 @@ def printBuspassgerList(busId):#liste des passger dans un bus specifique
             for passger in passengers:
                 print("==>  ID : {}   Name :  {}   Lugage weight :  {}".format(passger["id"],passger["name"].capitalize(),passger["lgageWeight"]))
 
+def allBusInfo():
+    print("___________Information sur tous les bus ____________")
+    print()
+    count = 0
+    for bus in myListBus:
+        print("==> ID :  {}  Possible places :  {}  Available places :  {}  number of Kg in bus :  {}".format(bus["id"],bus["availablePlace"],bus["availablePlace"] - len(bus["listPassenger"]),bus["nbreKg"]))
+        for pssger in bus["listPassenger"]:
+            print("        Id : {}  Name :  {}".format(pssger["id"],pssger["name"]))
+            count += 1
+    if count == 0 :
+        print("==> vous n'avez enregistrer aucun bus pour le moment")
+
+def BusInfo(busId):
+    for bus in myListBus:
+        if busId == bus["id"]:
+            print("==> ID :  {}  Possible places :  {}  Available places :  {}".format(bus["id"],bus["availablePlace"],bus["availablePlace"] - len(bus["listPassenger"]),bus["nbreKg"]))
+            for pssger in bus["listPassenger"]:
+                print("informations sur les passagers")
+                print("        Id : {}  Name :  {}  Lugage weight :  {} ".format(pssger["id"],pssger["name"],pssger["lgageWeight"]))
+
+def verifiedPassgerInBus(passengerName):
+    status=0
+    thebus={}
+    for bus in myListBus:
+        for pssger in bus["listPassenger"]:
+            if passengerName == pssger["name"]:
+                status = 1
+                thebus = bus
+    if status == 1:
+        print("==> le passager "+ passengerName +" est bien present dans ce bus ")
+        BusInfo(thebus["id"])
+    else:
+        print("==> Ce passager est encore vacan il n'est inscrit dans aucun bus")
+
+def verifiedTransfer(BusCommingId,BusGoingId):
+    placeUseInBuscomming=0
+    placeAvailableInBusGoing=0
+    commingBus={}
+    goingBus={}
+    status=0
+    for bus in myListBus:
+        if BusCommingId == bus["id"]:
+            commingBus = bus
+            status += 1
+        else:
+            goingBus = bus
+            status += 1
+    if status == 1 or status == 0 :
+        print("L'un des ID de bus renseigner est incorrect")
+    else:
+        placeUseInBuscomming = commingBus["availablePlace"] - len(commingBus["listPassenger"])
+        placeAvailableInBusGoing = goingBus["availablePlace"] - len(goingBus["listPassenger"])
+
+        if placeUseInBuscomming <= placeAvailableInBusGoing:
+            print("==> Le transfert de passager du bus {} vers le bus {} est possible".format(BusCommingId,BusGoingId))
+        else:
+            print("==> Le transfert de passager du bus {} vers le bus {} est impossible".format(BusCommingId,BusGoingId))
+
+def myswitcher(choix):
+
+    if choix == "a":
+        createBus()
+    elif choix == "b":
+        createPassenger()
+    elif choix == "c":
+        #allBusInfo()
+        response="r"
+        
+        print()
+        print("______________Tous les bus enregistree_______________")
+        print()
+        while response =="r":
+            for bus in myListBus:
+                print("==> ID :  {}  Possible places :  {}  Available places :  {}  number of Kg in bus :  {}".format(bus["id"],bus["availablePlace"],bus["availablePlace"] - len(bus["listPassenger"]),bus["nbreKg"]))
+            print()
+            id = input("==> Entrer l'id du bus :  ")
+            print()
+            print("______________Tous les bus enregistree_______________")
+            print()
+            printVacanListPassger()
+            name = input("==> Entrer le nom du passager a ajouter :  ")
+            addPassengerInBus(name,id)
+            response = input("Entrer r pour ajouter un autre passager et tout autre lettre pou sortir__:  ")
+    elif choix == "d":
+        #allBusInfo()
+        response="r"
+        print()
+        print("______________Tous les passagers vacans_______________")
+        print()
+        while response =="r":
+            for bus in myListBus:
+                print("==> ID :  {}  Possible places :  {}  Available places :  {}  number of Kg in bus :  {}".format(bus["id"],bus["availablePlace"],bus["availablePlace"] - len(bus["listPassenger"]),bus["nbreKg"]))
+            print()
+            id = input("==> Entrer l'id du bus :  ")
+            print()
+            print("______________Tous les bus enregistree_______________")
+            print()
+            printBuspassgerList(id)
+            name = input("==> Entrer le nom du passager a retirer :  ")
+            movePassengerInBus(name,id)
+            response = input("Entrer r pour ajouter un autre passager et tout autre lettre pou sortir__:  ")
+    elif choix == "e":
+        printVacanListPassger()
+    elif choix == "f":
+        #allBusInfo()
+        print()
+        print("______________Tous les bus enregistree_______________")
+        print()
+        for bus in myListBus:
+            print("==> ID :  {}  Possible places :  {}  Available places :  {}  number of Kg in bus :  {}".format(bus["id"],bus["availablePlace"],bus["availablePlace"] - len(bus["listPassenger"]),bus["nbreKg"]))
+        print()
+        id = input("==> Entrer l'id du bus :  ")
+        printBuspassgerList(id)
+    elif choix == "g":
+        name = input("==> Entrer le nom complet du passager :  ")
+        verifiedPassgerInBus(name)
+    elif choix == "h":
+        allBusInfo()
+    elif choix == "i":
+        for bus in myListBus:
+            print("==> ID :  {}  Possible places :  {}  Available places :  {}  number of Kg in bus :  {}".format(bus["id"],bus["availablePlace"],bus["availablePlace"] - len(bus["listPassenger"]),bus["nbreKg"]))
+        print()
+        idcomming = input("==> Entrer l'id du bus de provenance :  ")
+        idgoing = input("==> Entrer l'id du bus de destiation:  ")
+        verifiedTransfer(idcomming,idgoing)
+
+
+    
+
+
 
 #---------------------------------MAIN------------------------------------------------
-createBus()
-createPassenger()
-print(myListBus)
-printVacanListPassger()
-busId=input("enter l'id du bus   ")
-passger=input("enter entrer le nom du passager   ")
-print("1------------------------------------------------------------")
-addPassengerInBus(passger,busId)
-print("2------------------------------------------------------------")
-printBuspassgerList(busId)
-print("3------------------------------------------------------------")
-printVacanListPassger()
-print("4------------------------------------------------------------")
-movePassengerInBus(passger,busId)
-printBuspassgerList(busId)
-print("5------------------------------------------------------------")
-printVacanListPassger()
 
+choix = ""
+while response == "m":
+    print("----------------------GESTION DES PASSAGERS----------------------------")
+    print()
+    print(">     ENTRER \"A\" CREER D'UN BUS")
+    print(">     ENTRER \"B\" CREER UN PASSAGER")
+    print(">     ENTRER \"C\" AJOUTER UN PASSAGER DANS UN BUS")
+    print(">     ENTRER \"D\" RETIRER UN PASSAGER D'UN BUS")
+    print(">     ENTRER \"E\" AFFICHER LA LISTE DE TOUS LES PASSAGERS VACANS ")
+    print(">     ENTRER \"F\" AFFICHER LA LISTE DE TOUS LES PASSAGERS D'UN BUS")
+    print(">     ENTRER \"G\" VERIFIER SI UN PASSAGER EST ENREGISTRER DANS UN BUS")
+    print(">     ENTRER \"H\" INFORMATIONS SUR TOUS LES BUS CREER")
+    print(">     ENTRER \"I\" VERIFIER LE TRANSFER DE PASSAGER D'UN BUS A UN AUTRE")
+    print()
+    choix = input("==>   Enter votre choix___:  ")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    myswitcher(choix.lower())
+    response = input("==> Taper \"M\" pour rentrer au menu et tous autre caractere pour sortie du programme__:  ").lower()
 
 
 #---------------------------------MAIN------------------------------------------------
